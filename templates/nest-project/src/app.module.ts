3bbcd7fc383d346366da4c9f84c30b98f3033dc7
Luciano Gimenez <luciano.gimenezz21@gmail.com>
2024-06-02 16:42:30 -0300

    add: basic config for proyect with typeorm

--- templates/nest-project/src/app.module.ts
+++ templates/nest-project/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mssql'
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
